0ee3fbb1ba1135a128af0ea54e400fbc5c21376b
Charlie PAUVREé <chpauvre@gmail.com>
2021-02-24 10:12:15 +0100

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import images from './images';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={images.logo.src} className="App-logo" alt={images.logo.name} />
+        
       </header>
     </div>
   );
---
+++ src/images.tsx
@@ -0,0 +1,11 @@
+
+const baseURL = process.env.PUBLIC_URL + '/assets/images/';
+
+const images = {
+    logo: {
+        name: 'Logo',
+        src: baseURL + 'logo.png',
+    }
+}
+
+export default images;
