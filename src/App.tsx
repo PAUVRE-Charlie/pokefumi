55b9a624c49e8b133aa00b7e49efbd362824d4c0
Pierre SCHMUTZ <pierre.schmutz@imt-atlantique.net>
2021-02-24 13:35:08 +0100

    change app

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from './home';
 import {Connection} from './Connection'
 import {Matches} from './Matches'
 import Players from './Players';
+import Match from './Match';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path={process.env.PUBLIC_URL + '/connexion'} component={Connection} />
           <Route exact path={process.env.PUBLIC_URL + '/players'} component={Players} />
           <Route exact path={process.env.PUBLIC_URL + '/matches'} component={Matches} />
+          <Route exact path={process.env.PUBLIC_URL + '/match'} component={Match} />
         </body>
       </div>
     </Router>
