6ab04bc998ad1ff5e275c59ca7281a700177d2fe
Pierre Schmutz <schmutsi72@gmail.com>
2021-03-05 15:33:44 +0100

    change match view

--- src/Match.tsx
+++ src/Match.tsx
@@ -9,13 +9,21 @@ export default function Match() {
             </div>
 
             <div id='circleContainer'>
-                
-                    <a className='menuContainer' href="page1"><p>Choisir son pokémon</p><img id='circle' src={images.circle.src} alt="circle"/></a>
-                
-                    <a className='menuContainer' href="page2">Match en cours<br/><img id='circle' src={images.circle.src} alt="circle"/></a>
-               
-                    <a className='menuContainer' href="page">Résultats<br/><img id='circle' src={images.circle.src} alt="circle"/></a>
-               
+                <div className='menuContainer'>
+                    <a href="page1">Choisir son pokémon<br/><img id='circle' src={images.circle.src} alt="circle"/></a>
+                </div>
+                <div>
+                    <p style={{fontSize:'40px'}}>_____________________</p>
+                </div>
+                <div>
+                    <p>Match en cours<br/><img id='circle' src={images.circle.src} alt="circle"/></p>
+                </div>
+                <div>
+                    <p style={{fontSize:'40px'}}>_____________________</p>
+                </div>
+                <div>
+                    <a href="page3">Résultats<br/><img id='circle' src={images.circle.src} alt="circle"/></a>
+                </div>
             </div>
 
             <div className="informationContainer" style={{marginTop:'25vh'}}>
